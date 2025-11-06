057e7fa0d0ed92a15e7e2e76d25e95eac6b69aa5
Ianfsl <ian.lourenco13@outlook.com>
2025-11-06 20:54:27 -0300

    Ajustando

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      "http://localhost:3000",
-      "http://localhost:5173",
+      "https://localhost:3000",
       "https://localhost:5173",
-      "http://127.0.0.1:5173",
+      "https://localhost:5173",
+      "https://127.0.0.1:5173",
       "https://pi-guia-turistico-backend.onrender.com",
       "https://joiasdeitaqueri.netlify.app/",
       process.env.FRONTEND_URL,
